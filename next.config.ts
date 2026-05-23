c41a016b926bbacac11fae20bedda6b27fae94c7
ifdadigitalai-wq <ifdadigitalai@gmail.com>
2026-05-23 14:52:38 +0530

    Added logo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // Add this line to fix the local logo on Android
     remotePatterns: [
       {
         protocol: "https",
